48929489cfd4c34c00906cacad9a9f4eddb210c3
Ruurd Keizer <ruurdk@hotmail.com>
2023-02-21 17:31:28 +0100

    update apt packages

--- infra/scripts/instance.sh
+++ infra/scripts/instance.sh
@@ -36,6 +36,7 @@ echo "$(date) - PREPARE done" >> /home/ubuntu/install.log
 echo "$(date) - INSTALLING docker" >> /home/ubuntu/install.log
 
 # Install docker
+apt-get install software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
 apt-cache policy docker-ce
