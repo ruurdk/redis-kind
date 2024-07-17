f4aef3e88a50336be84158486950ac076c9179f6
Ruurd Keizer <ruurdk@hotmail.com>
2024-07-17 16:19:04 +0200

    retry on cluster create

--- create-kind-clusters.sh
+++ create-kind-clusters.sh
@@ -6,7 +6,8 @@ source config.sh
 for c in $(seq 1 $num_clusters);
 do
     echo "$(date) - Creating cluster $c"
-    kind create cluster --config kind-cluster-c$c.yaml
+    while ! kind create cluster --config kind-cluster-c$c.yaml ; do echo "Attempting to create cluster $c again after failure." ; done
+    
 
     # Echo endpoints
     kubectl cluster-info --context kind-c$c
