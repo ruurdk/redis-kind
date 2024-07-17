d3f74976edbd9082cc71bb548beaec03c80e8d6f
Ruurd Keizer <ruurdk@hotmail.com>
2024-07-17 17:25:13 +0200

    secret and db separate

--- deploy-db.sh
+++ deploy-db.sh
@@ -8,6 +8,8 @@ then
     echo "$(date) - DEPLOYING Redis Enterprise A/A database on k8s cluster 1" 
 
     kubectl config use-context kind-c1
+    # do the below in 2 steps or the admission controller will deny it
+    kubectl apply -f reaadb_secret.yaml
     kubectl apply -f reaadb.yaml
 fi
 
