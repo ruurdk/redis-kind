073bda15b1c8b1633132c33af22bc65a831b57c9
Ruurd Keizer <ruurdk@hotmail.com>
2024-09-16 20:14:46 +0200

    validation on rza workers

--- sanity-check.sh
+++ sanity-check.sh
@@ -5,6 +5,50 @@ source config.sh
 
 echo "$(date) - Checking input."
 
+# K8s nodes.
+if [ "$worker_nodes" -lt 1 ] || [ $((worker_nodes%2)) -eq 0 ];
+then
+  echo "$(date) - ERROR: The number of worker nodes ($worker_nodes) should be a positive odd number."
+  exit 1
+else
+  echo "$(date) - Validated number of worker nodes: $worker_nodes."
+fi
+if [ "$control_nodes" -lt 1 ] || [ $((control_nodes%2)) -eq 0 ];
+then
+  echo "$(date) - ERROR: The number of control plane nodes ($control_nodes) should be a positive odd number."
+  exit 1
+else
+  echo "$(date) - Validated number of control plane nodes: $control_nodes."
+fi
+
+# RZA.
+if [ "$rackzone_aware" == "yes" ]; 
+then
+  if [ "$rackzone_zones" -lt 1 ] || [ $((rackzone_zones%2)) -eq 0 ];
+  then
+    echo "$(date) - ERROR: The number of rack zones ($rackzone_zones) should be a positive odd number."
+    exit 1
+  else
+    echo "$(date) - Validated number of rack zone: $rackzone_zones."
+  fi
+
+  if [ "$worker_nodes" -lt "$rackzone_zones" ];
+  then
+    echo "$(date) - ERROR: The number of worker nodes ($worker_nodes) is not capable of hosting all rack zones ($rackzone_zones)."
+    exit 1
+  else
+    echo "$(date) - Validated the number of worker nodes ($worker_nodes) is capable of hosting all rack zones ($rackzone_zones)."    
+  fi
+  ideal_workers=$((rackzone_zones*2 - 1))
+  if [ ! "$ideal_workers" -eq "$worker_nodes" ];
+  then
+    echo "$(date) - WARN: The number of worker nodes ($worker_nodes) is sub optimal for rack zones ($rackzone_zones). Ideal number of worker nodes: $ideal_workers."    
+  else
+    echo "$(date) - Validated the number of worker nodes ($worker_nodes) is ideal for the number of rack zones ($rackzone_zones)."    
+  fi
+fi
+
+# Number of clusters.
 if [ "$num_clusters" -lt 1 ]; then
     echo "$(date) - ERROR: The number of clusters should be at least equal to 1."
     exit 1
