65d90c5e398245ad7ede972696999c333de473f4
Ruurd Keizer <ruurdk@hotmail.com>
2024-09-17 20:04:42 +0200

    k8s dashboard user

--- help.sh
+++ help.sh
@@ -16,16 +16,24 @@ echo "There is internal (in KinD) connectivity through Docker networks"
 echo "In case you need to access ports on the host/outside (eg. admin GUI at 8443) on cluster N: "
 echo "              kubectl port-forward --address localhost,0.0.0.0 svc/recN-ui 8443:8443 -n redis"
 echo "Other interesting ports may be prometheus (9090), Grafana (3000), Ingress (443) or the local database port."
+if [ "$install_dashboard" == "yes" ];
+then
+echo "Create a token for the k8s dashboard with:"
+echo "              kubectl -n kubernetes-dashboard create token admin-user"
+fi
 
 # Print how to exec rladmin.
 echo "_______________________________________"
 echo "Accessing the admin CLI (status) on cluster N: "
 echo "              kubectl exec -it pod/recN-0 -n redis -- rladmin status"
 
-# Test A/A connectivity.
-echo "_______________________________________"
-echo "Test A/A end to end connectivity:"
-echo "              Run the debug/testlink.sh script (it leverages redis-cli in a container)."
+if [ "$active_active" == "yes" ];
+then
+    # Test A/A connectivity.
+    echo "_______________________________________"
+    echo "Test A/A end to end connectivity:"
+    echo "              Run the debug/testlink.sh script (it leverages redis-cli in a container)."
+fi
 
 # K8s nodes access.
 echo "_______________________________________"
--- kubeinfra/dashboard/install_dashboard.sh
+++ kubeinfra/dashboard/install_dashboard.sh
@@ -11,4 +11,7 @@ do
     helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
     # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
     helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+    # Create a service account and role binding.
+    kubectl apply -f admin-user.yaml
 done
