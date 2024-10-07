a955507d39bad94d61392fffb88590c9994e8302
ruurdk <ruurdk@hotmail.com>
2024-10-07 21:26:32 +0200

    switch to redis-benchmark-go and update default database configs

--- apps/redis-benchmark/redis-benchmark.sh
+++ apps/redis-benchmark/redis-benchmark.sh
@@ -24,20 +24,12 @@ do
     SVCPASS=`kubectl get secret/${DBSECNAME} -o jsonpath='{.data.password}' | base64 --decode`
     SVCUSER=`kubectl get secret/${DBSECNAME} -o jsonpath='{.data.username}' | base64 --decode`
 
-    # TODO: 
-    #   - make this continuous output vs. 1 run every 120s
-    #   - wrap in proper deployment    
-    cat << EOF | kubectl apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: memtier
-spec:
-  containers:
-  - name: memtier
-    image: redislabs/memtier_benchmark:latest
-    args: ["--tls","--tls-skip-verify","--key-prefix=a","--ratio=1:4","--test-time=120","-d","100","-t","2","-c","25","--pipeline=50","--key-pattern=S:S","--hide-histogram","-x","1000","-s","${SVCNAME}.redis.svc","-p","${SVCPORT}"]
-EOF
+    # get benchmark deployment from template and edit arguments
+    cp redis-benchmark_template.yaml redis-benchmark.yaml
+    args=[\"-tls\",\"-tls-skip\",\"-d\",\"100\",\"-c\",\"25\",\"-h\",\"${SVCNAME}.redis.svc\",\"-p\",\"${SVCPORT}\",\"-rps\",\"5000\",\"-l\",\"SET\",\"__key__\",\"__data__\"]
+    yq -iy ".spec.template.spec.containers[0].args = $args" redis-benchmark.yaml
+
+    kubectl apply -f redis-benchmark.yaml
 
 done
 
