a2cdce92a2adebed9ba5c3e99fd85537ab33d6f6
Kamran Yousaf <kamran-redis@users.noreply.github.com>
2025-04-25 11:41:56 +0100

    changed for ineteractive2

--- prereqs/install-docker-linux.sh
+++ prereqs/install-docker-linux.sh
@@ -33,7 +33,7 @@ esac
 
 case $ID in
     "ubuntu")
-        DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip
+        DEBIAN_FRONTEND=noninteractive apt-get install -yq python3-pip
         pip install  yq
         ;;
     "debian")
